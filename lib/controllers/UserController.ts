745c1ee1ce867584ee236ef26cab570a4e6f5eff
Robin Louis van Komen <rlvk@typoconsult.dk>
2019-03-10 17:40:26 +0100

    initial commit with basic setup

---
+++ lib/controllers/UserController.ts
@@ -0,0 +1,55 @@
+import * as mongoose from 'mongoose';
+import { Request, Response } from 'express';
+
+import { UserSchema } from '../models/UserModel';
+
+const User = mongoose.model('User', UserSchema);
+
+export class UserController {
+    public addNewUser(req: Request, res: Response) {
+        let newUser = new User(req.body);
+
+        newUser.save((err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(user);
+        });
+    }
+
+    public getUsers(req: Request, res: Response) {
+        User.find({}, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(user);
+        });
+    }
+
+    public getUserById(req: Request, res: Response) {
+        User.findById(req.params.userId, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(user);
+        });
+    }
+
+    public updateUser(req: Request, res: Response) {
+        User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true }, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(user);
+        });
+    }
+
+    public deleteUser(req: Request, res: Response) {
+        User.remove({ _id: req.params.userId }, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'Successfully deleted user!' });
+        });
+    }
+}
---
+++ lib/routes/routes.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+import { UserController } from "../controllers/UserController";
+
+export class Routes {
+    public userController: UserController = new UserController();
+
+    public routes(app): void {
+        app.route('/')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET request successfulll!!!!'
+                })
+            })
+
+        // User 
+        app.route('/user')
+            .get(this.userController.getUsers)
+            .post(this.userController.addNewUser)
+
+        app.route('/user/:userId')
+            .get(this.userController.getUserById)
+            .put(this.userController.updateUser)
+            .delete(this.userController.deleteUser)
+    }
+}
