551629c67686a8f13227da41f0fef5ee7e4732aa
Robin Louis van Komen <rlvk@typoconsult.dk>
2019-03-15 18:37:30 +0100

    codeship testing

--- lib/server.ts
+++ lib/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+    console.log('Changed... ' + PORT);
 })
