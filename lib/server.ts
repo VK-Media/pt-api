7b23b2545e1cb9edbc94698c084f66eef7b0674c
Omvk97 <41872656+Omvk97@users.noreply.github.com>
2019-03-18 11:30:57 +0100

    Update server.ts

--- lib/server.ts
+++ lib/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('Server is running on: ' + PORT);
+    console.log('Robin er lækker: ' + PORT);
 })
