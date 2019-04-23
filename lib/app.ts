9b49c70b005b9b529ddaf884842c43d45a17a8b3
Robin Louis van Komen <robin@vkmedia.dk>
2019-04-23 18:02:59 +0200

    add access methods

--- lib/app.ts
+++ lib/app.ts
@@ -28,6 +28,7 @@ class App {
         this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
             next();
         });
     }
